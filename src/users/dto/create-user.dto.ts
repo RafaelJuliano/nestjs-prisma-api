0297324b8739a13f5adfab3fea17888e7468bc5b
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2023-07-01 08:54:33 -0300

    Created User entity

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsEmail, IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  admin: boolean;
+}
